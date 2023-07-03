eeea05a4a0d77b060189b75b9fcbee9c87d6d6b6
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-07-03 22:18:05 +0200

    Aggiungere i file di progetto.

---
+++ That sums it up/Program.cs
@@ -0,0 +1,12 @@
+﻿List<double> listOfnumbers = new(){3.14, 2.72, 63.00, 84.7, 8.9, 4.0, 23.12};
+
+double sum= listOfnumbers.Sum(x=>x);
+double average= listOfnumbers.Average();
+int countElementOfList= listOfnumbers.Count();
+
+Console.WriteLine("somma di tutti gli elementi della lista: " + sum);
+Console.WriteLine("media della lista: " + average);
+Console.WriteLine("la lista ha " + countElementOfList + " elementi");
+
+
+
